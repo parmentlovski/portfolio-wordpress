c5b17fbe61e1a263633a04f10a160cfb8ae66d2d
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-08-06 15:29:34 +0200

    Insertion section services

--- front-page.php
+++ front-page.php
@@ -1,8 +1,40 @@
 <?php do_action('display_header', 'package_header'); ?>
 
-<main class="container">
+<main class="container mt-6">
 
-   
+    <section id="services" class="m-6">
+
+        <h2 class="is-size-1 mt-6">SERVICES</h2>
+
+        <div class="content-services columns">
+
+            <img class="img-services mt-6 services" src="wp-content/themes/from_scratch/assets/img/services.png">
+
+            <div class="box-services is-half">
+                <div class="card-services">
+                    <h3>Service n°1</h3>
+                    <p>blablablablaalbalblalblabllbalbaablalblaba</p>
+                    <img src="wp-content/themes/from_scratch/assets/img/programming.png">
+                </div>
+                <div class="card-services">
+                    <h3>Service n°1</h3>
+                    <p>blablablablaalbalblalblabllbalbaablalblaba</p>
+                    <img src="wp-content/themes/from_scratch/assets/img/programming.png">
+                </div>
+                <div class="card-services">
+                    <h3>Service n°1</h3>
+                    <p>blablablablaalbalblalblabllbalbaablalblaba</p>
+                    <img src="wp-content/themes/from_scratch/assets/img/programming.png">
+                </div>
+                <div class="card-services">
+                    <h3>Service n°1</h3>
+                    <p>blablablablaalbalblalblabllbalbaablalblaba</p>
+                    <img src="wp-content/themes/from_scratch/assets/img/programming.png">
+                </div>
+            </div>
+
+        </div>
+    </section>
 </main>
 
 <?php get_footer();
