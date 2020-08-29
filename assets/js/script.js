bc27ea638be1b135a3b18fa0ed484b5903e7849e
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-08-30 00:49:41 +0200

    Ajout animation section projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-window.onload = function(){
+// window.onload = function(){
     // TweenMax.set('body', {opacity: 1});
     let tl1 = new TimelineMax();
 
     tl1
-        .from   ('.img-header', 2.6, {opacity:0, ease: Bounce. easeOut , y:-300})
+        .from   ('.img-header', 2.6, {opacity:0, ease: Bounce.easeOut , y:-300})
         // .to('.img-header', 1, { ease: Back.easeOut.config(2.7)})
 
 
@@ -27,19 +27,22 @@ window.onload = function(){
 //     // //     animation = false;   
 //     // // }, 1500);
 //     // })
-}
+// }
 
 // ANIMATION SERVICES //
 
 var controller = new ScrollMagic.Controller();
-var tl2 = new TimelineMax();
-tl2.from('.img-services', 0.25, {opacity:0, x:-150, ease: Power0.easeOut});   
+var tlService = new TimelineMax();
+tlService
+  .from('.img-services', 0.2, {opacity:0, x:-150, ease: Power0.easeOut})  
+  .staggerFrom('.card-services', 0.3, {y:50, opacity: 0}, 0.3)
+   
 
 var scene = new ScrollMagic.Scene({
     triggerElement: "#services",
     triggerHook: "onLeave",
   })
-    .setTween(tl2)
+    .setTween(tlService)
     .addTo(controller);
 
 
@@ -50,7 +53,10 @@ var btn = tr.lastChild;
 
 var controller2 = new ScrollMagic.Controller();
 var tl3 = new TimelineMax();
-tl3.from('.img-contact', 0.25, {opacity:0, delay: 0.75, x:-150, ease: Power0.easeOut});
+tl3
+  .from('.box-form', 0.1, {delay: 1, scale:1, opacity:0})
+  .from('.img-contact', 0.1, {opacity:0, delay: 0.1, x:-150, ease: Power0.easeOut})
+  .from('#map', 0.1, {opacity:0, x:150, ease: Power0.easeOut})
 
 var scene2 = new ScrollMagic.Scene({
     triggerElement: btn,
@@ -62,24 +68,24 @@ var scene2 = new ScrollMagic.Scene({
 
 // ANIMATION SUPERMAN //
 
-var controller3 = new ScrollMagic.Controller();
+// var controller3 = new ScrollMagic.Controller();
 
-    jQuery('#projets').each(function() {
-      var tl = new TimelineMax();
-      var child = jQuery(this).find(".img-projets");
-      tl.to(child, 0.1, { y: -880, ease: Linear.easeInOut });
+//     jQuery('#projets').each(function() {
+//       var tl = new TimelineMax();
+//       var child = jQuery(this).find(".img-projets");
+//       tl.to(child, 0.1, { y: -880, ease: Linear.easeInOut });
     
-      var scene3 = new ScrollMagic.Scene({
-        triggerElement: this,
-        triggerHook: 0.4,
-        duration: "130%"
-      })
-        .setTween(tl)
-        .addTo(controller3);
-    });
+//       var scene3 = new ScrollMagic.Scene({
+//         triggerElement: this,
+//         triggerHook: 0.4,
+//         duration: "130%"
+//       })
+//         .setTween(tl)
+//         .addTo(controller3);
+//     });
 
   
-  // ANIMATION H1 HEADER //
+//   // ANIMATION H1 HEADER //
   
   const allLetters = document.querySelectorAll('.animationLetters span');
   // const ligne = document.querySelector('.ligne');
@@ -91,4 +97,21 @@ var controller3 = new ScrollMagic.Controller();
     .from(allLetters, {opacity: 0, top: gsap.utils.wrap([35, -35]), stagger: 0.09, ease: "power2.out"})
     // .set(ligne, {width: '34vw'});
 
-});
+// Animation Projet 
+
+var controllerProjet = new ScrollMagic.Controller();
+let tlProjet = new TimelineMax();
+
+tlProjet
+  .staggerFrom('.item', 0.1, {y:50, opacity: 0}, 0.3)
+  .staggerFrom('.line', 0.1, {opacity: 0})
+  .from('.content-heart', 1.5, {delay: 0.1, scale:0, opacity:0, ease: Back.easeOut.config(2.5)})
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#projets",
+    triggerHook: "onLeave",
+  })
+    .setTween(tlProjet)
+    .addTo(controllerProjet);
+  
+  });
