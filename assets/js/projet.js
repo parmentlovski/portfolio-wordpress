c8b6443d1dd17e59d5ba106b4782a8a093b8c4a1
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-08-30 00:49:18 +0200

    Ajout animation section projet

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -1,19 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// var competences = document.querySelectorAll('.competences');
-
-// competences.forEach(competence => {
-
-//   let val = competence.trim().split(" "); 
- 
-//   if(val.length > 0) {
-//     val.parent().hide();
-//   } 
-
-// });
-
-
-jQuery(".stringCompetences").each(function(){ 
+  jQuery(".stringCompetences").each(function(){ 
   if(jQuery(this).html().trim()==="") jQuery(this).parent().hide();
 });
 
