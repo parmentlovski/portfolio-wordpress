a7a2edde53d7f32b84077cdc30db8217e7f8869a
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-08-30 20:23:51 +0200

    Ajout d'une div pour contenir les réseaux sociaux

--- footer-frontpage.php
+++ footer-frontpage.php
@@ -10,12 +10,14 @@
       </div>
       <p>Création de site internet - Besançon - 2020 </p>
       <a id="mentions" href="mentions-legales" target="_blank">Mentions légales</a>
-      <a href="https://fr.linkedin.com/in/bryan-parmentelot" class="reseaux" target="_blank">
-         <img src="wp-content/themes/from_scratch/assets/img/linkedin.svg" alt="Logo du Linkedin de Parmentelot Bryan">
-      </a>
-      <a href="https://github.com/parmentlovski" class="reseaux" target="_blank">
-         <img src="wp-content/themes/from_scratch/assets/img/github.svg" alt="Logo du Github de Parmentelot Bryan">
-      </a>
+      <div class="content-reseaux">
+         <a href="https://fr.linkedin.com/in/bryan-parmentelot" class="reseaux" target="_blank">
+            <img src="wp-content/themes/from_scratch/assets/img/linkedin.svg" alt="Logo du Linkedin de Parmentelot Bryan">
+         </a>
+         <a href="https://github.com/parmentlovski" class="reseaux" target="_blank">
+            <img src="wp-content/themes/from_scratch/assets/img/github.svg" alt="Logo du Github de Parmentelot Bryan">
+         </a>
+      </div>
    </div>
 
 </footer>
