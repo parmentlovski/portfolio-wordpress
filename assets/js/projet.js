fa283c09b85689618d0236c0c2bda7f1de0332a3
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-09-02 19:52:18 +0200

    fonctionnalité trim projet ok

--- assets/js/projet.js
+++ assets/js/projet.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  jQuery(".stringCompetences").each(function(){ 
-  if(jQuery(this).html().trim()==="") jQuery(this).parent().hide();
-});
+
 
 });
