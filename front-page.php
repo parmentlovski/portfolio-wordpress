e7099766ac84afa3e6f77a8349404fc91b9e135f
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-09-02 19:56:04 +0200

    Ajout du message email envoye dans le formulaire

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
             <div class="box-services columns is-mobile is-multiline">
                 <div class="card-services animation-services column is-offset-1-mobile is-10-mobile is-offset-1-desktop is-11-desktop mt-6">
                     <h3 class="mt-5">
-                        <img src="wp-content/themes/from_scratch/assets/img/design.png" class="mr-4" alt="Logo représentant le design web" width="64px" height="64px">
+                        <img src="wp-content/themes/from_scratch/assets/img/design.png" class="mr-4" alt="Logo représentant le design web" width="64" height="64">
                         <span>Design</span></h3>
                     <h4 class="mt-6 mb-5">“Mettons en valeur votre identité en répondant à vos objectifs“</h4>
                     <p>
@@ -19,16 +19,14 @@
                     <p>Je dispose des compétences permettant de mettre en valeur l'identité de votre projet : allant du template acheté au design sur mesure.
                     </p>
                     <p>Selon votre budget nous déciderons ensemble de la solution la plus adéquate.</p>
-                    </p>
                 </div>
 
                 <div class="animation-services line-services-horizontal">
                 </div>
-                <!-- <img class="img-services mt-6 column is-half services" src="wp-content/themes/from_scratch/assets/img/services.png" alt="Image représentant le service" width="1256px" height="1588px"> -->
 
                 <div class="card-services animation-services column is-offset-1-mobile is-10-mobile is-offset-1-desktop is-11-desktop">
                     <h3>
-                        <img src="wp-content/themes/from_scratch/assets/img/repair.png" class="mr-4" alt="Logo représentant le développement web" width="64px" height="64px"> <span>Développement</span></h3>
+                        <img src="wp-content/themes/from_scratch/assets/img/repair.png" class="mr-4" alt="Logo représentant le développement web" width="64" height="64"> <span>Développement</span></h3>
                     <h4 class="mt-6 mb-5">“La clef de voûte de votre site internet“</h4>
                     <p>
                         Selon vos besoins et vos objectifs je propose différentes solutions :
@@ -45,7 +43,7 @@
 
                 <div class="card-services animation-services column is-offset-1-mobile is-10-mobile is-offset-1-desktop is-11-desktop">
                     <h3>
-                        <img src="wp-content/themes/from_scratch/assets/img/vision.png" class="mr-4" alt="Logo représentant la méthodologie" width="64px" height="64px">
+                        <img src="wp-content/themes/from_scratch/assets/img/vision.png" class="mr-4" alt="Logo représentant la méthodologie" width="64" height="64">
                         <span>Procédé</span></h3>
                     <h4 class="mt-6 mb-5">“Ensemble, nous répondrons à vos besoins“</h4>
 
@@ -81,8 +79,6 @@
                     'post_type' => 'post',
                     'showposts' => -1,
                     'cat' => 2,
-                    // 'posts_per_page' => 1,
-                    // 'order' => 'ASC', // classé par ordre alphabétique
                     'orderby' => array('date_post' => 'ASC'), // par titre
                 );
 
@@ -95,20 +91,13 @@
                 ?>
                         <div class="projet item column is-offset-1-mobile is-10-mobile is-5-tablet">
 
-
                             <h3 class="has-text-centered is-size-3 mb-4"><?php the_title(); ?></h3>
 
-                            <div id="content-placement" class="columns is-mobile my-5">
+                            <div class="content-placement columns is-mobile my-5">
 
-                                <!-- <div class="column is-1 arrow">
-                                        <img src="wp-content/themes/from_scratch/assets/img/left.png" class="left" alt="Logo représentant une flèche allant dans la direction de gauche" height="32px" width="32px">
-                                    </div> -->
-
-
-                                <div class="has-text-centered column is-offset-1-mobile is-10-mobile is-offset-1-tablet is-4-tablet content-img-projet" style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>');">
-                                    <!-- <img src="wp-content/themes/from_scratch/assets/img/bysshe.png" alt="Image du projet Bysshe" height="" width=""> -->
+                                <div class="has-text-centered column is-half-mobile is-offset-1-tablet is-4-tablet content-img-projet" style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>');">
                                 </div>
-                                <div class="content-competences column is-offset-1-mobile is-10-mobile is-half-tablet ">
+                                <div class="content-competences column is-half-mobile is-half-tablet ">
                                     <div class="columns is-multiline">
 
                                         <?php
@@ -142,129 +131,126 @@
                                     </div>
 
                                 </div>
-
-                                <!-- <div class="column is-1 arrow">
-                                        <img src="wp-content/themes/from_scratch/assets/img/right.png" class="right somewhere_projet a_la_suite_projet" alt="Logo représentant une flèche allant dans la direction de droite" height="32px" width="32px">
-                                    </div> -->
                             </div>
-                            <p class="my-4 descriptifProjet"><?php the_excerpt(); ?></p>
+                            <div class="my-4 descriptifProjet"><?php the_excerpt(); ?></div>
                             <p><?php the_content(); ?></p>
-                            <!-- <hr> -->
                         </div>
                 <?php
                     endwhile;
                 endif; ?>
-                <img class="img-projet item column is-5" src="wp-content/themes/from_scratch/assets/img/online_presentation.png" alt="Image représentant les projets" width="1574px" height="1180px">
+                <img class="img-projet item column is-5" src="wp-content/themes/from_scratch/assets/img/presentation_projet.svg" alt="Image représentant les projets">
             </div>
-            <!-- <div class="box-img column is-half">
-                <img class="img-projets" src="wp-content/themes/from_scratch/assets/img/super_man.svg">
-            </div> -->
         </div>
 
     </section>
 
-<section id="contact" class="mt-6">
-    <h2 class="is-size-2 is-size-3-mobile mt-10 titleSection titleWhite"><span class="backgroundYellow titleContact mt-6">CONTACT</span></h2>
+    <section id="contact" class="mt-6">
 
-    <div class="content-form columns">
+        <h2 class="is-size-2 is-size-3-mobile mt-10 titleSection titleWhite"><span class="backgroundYellow titleContact mt-6">CONTACT</span></h2>
 
-        <div class="column is-offset-1 is-3-fullhd content-img">
-            <img class="img-contact" src="wp-content/themes/from_scratch/assets/img/mail2.svg">
+        <div class="content-form columns">
+
+            <div class="column is-offset-1-fullhd is-3-fullhd content-img">
+                <img class="img-contact" src="wp-content/themes/from_scratch/assets/img/formulaire_contact.svg" alt="Image représentant un mail envoyé via le formulaire de contact">
+            </div>
+
+
+            <div class="box-form column is-auto-tablet is-4-fullhd">
+
+                <form action="#" id="contactForm" method="post">
+
+                    <div id="content-input" class="columns is-multiline is-mobile">
+
+                        <div class="help-block with-errors column is-12-mobile is-offset-desktop is-11-desktop">
+                            <div class="box-input columns is-mobile input-erreur">
+                                <span class="input-logo column is-1"><i class="fas fa-exclamation-circle" aria-hidden="true"></i></span>
+                                <span id="erreur" class="erreur column is-11"></span>
+                            </div>
+                        </div>
+
+                        <div id="sendEmail" class="column is-12-mobile is-offset-1-desktop is-11-desktop ">
+                        <?php
+                                    if (isset($_GET['email_transmis']) && $_GET['email_transmis'] === "oui") {?>
+                            <div class="box-input columns is-mobile input-email-valide">
+                            <span class="input-logo column is-1"><i class="far fa-check-circle" aria-hidden="true"></i></span>
+                            <span class="valide column is-11">
+                                           <?php echo 'E-mail envoyé';?>
+                                           </span>
+                            </div>
+                             <?php   }  ?>
+                            <?php if (isset($_GET['email_transmis']) && $_GET['email_transmis'] === "non") { ?>
+                            <div class="box-input columns is-mobile input-email-non-valide">
+                                <span class="input-logo column is-1"><i class="far fa-times-circle" aria-hidden="true"></i></span>
+                                <span class="non-valide column is-11">
+                                <?php
+                                    echo 'Problème de serveur';?>
+                                    </span>
+                            </div>
+                              <?php  } ?>
+                        </div>
+
+                        <div class="column is-12-mobile is-offset-1-desktop is-11-desktop ">
+                            <div class="box-input columns is-mobile">
+                                <span class="input-logo column is-1"><i class="fa fa-user" aria-hidden="true"></i></span>
+                                <input type="text" id="name" class="column is-10" name="name" placeholder="Nom / prénom">
+                                <span class="input-obligatoire column is-1"><i class="fas fa-asterisk"></i></span>
+                            </div>
+                            <span id="erreurName" class="erreur"></span>
+                        </div>
+
+                        <div class="column is-12-mobile is-offset-1-desktop is-11-desktop">
+                            <div class="box-input columns is-mobile">
+                                <span class="input-logo column is-1"><i class="fas fa-at" aria-hidden="true"></i></span>
+                                <input type="email" name="email" class="column is-10" id="email" placeholder="E-mail">
+                                <span class="input-obligatoire column is-1"><i class="fas fa-asterisk"></i></span><br>
+                            </div>
+                            <span id="erreurEmail" class="erreur"></span>
+
+                        </div>
+
+                        <div class="column is-12-mobile is-offset-1-desktop is-11-desktop">
+                            <div class="box-input columns is-mobile">
+                                <span class="column is-1 input-logo-tel"><i class="fas fa-phone" aria-hidden="true" style="transform: rotate(100deg);"></i></span>
+                                <input type="tel" id="tel" class="column is-11" name="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" placeholder="Téléphone">
+                            </div>
+                            <span id="erreurTel" class="erreur"></span>
+                        </div>
+
+                        <div class="column is-12-mobile is-offset-1-desktop is-11-desktop">
+                            <div class="box-input columns is-mobile input-textarea">
+                                <textarea id="message" name="message" class="column is-11" placeholder="Merci de détailler vos besoins."></textarea>
+                                <span class="input-obligatoire-message column is-1"><i class="fas fa-asterisk"></i></span>
+                            </div>
+                            <span id="erreurMessage" class="erreur"></span>
+
+
+                        </div>
+
+                        <div class="send column is-12 form-button">
+                            <button name="message-submit" type="submit" id="form-submit" class="btnSend ml-6"><span>Envoyer</span></button>
+                            <input type="hidden" name="hidden" value="1">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="map column is-auto-tablet is-2-fullhd" id="map">
+                <!-- Ici s'affichera la carte -->
+            </div>
         </div>
 
-
-        <div class="box-form column is-4-fullhd">
-
-            <form action="#" id="contactForm" method="post">
-                <!-- <legend class="text-center">Me contacter</legend> -->
-
-                <div id="content-input" class="columns is-multiline is-mobile">
-
-                    <div class="help-block with-errors column is-12-mobile is-offset-desktop is-11-desktop">
-                        <div class="box-input columns is-mobile input-erreur">
-                            <span class="input-logo column is-1"><i class="fas fa-exclamation-circle" aria-hidden="true"></i></span>
-                            <span id="erreur" class="erreur column is-11"></span>
-                        </div>
-                    </div>
-
-                    <div class="column is-12-mobile is-offset-1-desktop is-11-desktop ">
-                        <div class="box-input columns is-mobile">
-                            <!-- <img src="wp-content/themes/from_scratch/assets/img/user.png" class="formLogo" alt="Logo pour le champ du formulaire prénom" width="16px" height="16px"> -->
-                            <span class="input-logo column is-1"><i class="fa fa-user" aria-hidden="true"></i></span>
-                            <input type="text" id="name" class="column is-10" name="name" placeholder="Nom / prénom">
-                            <span class="input-obligatoire column is-1"><i class="fas fa-asterisk"></i></span>
-                        </div>
-                        <span id="erreurName" class="erreur"></span>
-                    </div>
-
-                    <div class="column is-12-mobile is-offset-1-desktop is-11-desktop">
-                        <div class="box-input columns is-mobile">
-                            <span class="input-logo column is-1"><i class="fas fa-at" aria-hidden="true"></i></span>
-                            <input type="email" name="email" class="column is-10" id="email" placeholder="E-mail">
-                            <span class="input-obligatoire column is-1"><i class="fas fa-asterisk"></i></span><br>
-                        </div>
-                        <span id="erreurEmail" class="erreur"></span>
-
-                    </div>
-
-                    <div class="column is-12-mobile is-offset-1-desktop is-11-desktop">
-                        <div class="box-input columns is-mobile">
-                            <span class="column is-1 input-logo-tel"><i class="fas fa-phone" aria-hidden="true" style="transform: rotate(100deg);"></i></span>
-                            <input type="tel" id="tel" class="column is-11" name="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" placeholder="Téléphone">
-                        </div>
-                        <span id="erreurTel" class="erreur"></span>
-                    </div>
-
-                    <!-- <div class="column is-offset-2 is-4">
-                            <input type="text" id="object" name="object" placeholder="Objet *"><br>
-                            <span id="erreurObject" class="erreur"></span>
-                        </div> -->
-
-                    <div class="column is-12-mobile is-offset-1-desktop is-11-desktop">
-                        <div class="box-input columns is-mobile input-textarea">
-                            <textarea id="message" name="message" class="column is-11" placeholder="Merci de détailler vos besoins."></textarea>
-                            <span class="input-obligatoire-message column is-1"><i class="fas fa-asterisk"></i></span>
-                        </div>
-                        <span id="erreurMessage" class="erreur"></span>
+    </section>
 
 
-                    </div>
-
-                    <div class="send column is-12 form-button">
-                        <button name="message-submit" type="submit" id="form-submit" class="btnSend ml-6"><span>Envoyer</span></button>
-                        <input type="hidden" name="hidden" value="1">
-                    </div>
-                </div>
-
-
-
-            </form>
-
-            <!-- <svg id="Composant_1_1" data-name="Composant 1 – 1" xmlns="http://www.w3.org/2000/svg" width="770.513" height="544.875" viewBox="0 0 770.513 544.875" style="margin-bottom: -6%;">
-                    <path id="blob_menu" data-name="blob menu" d="M67.017,42.622c87.077-95.9,292.155,0,292.155,0v674.09S134.3,827.447,67.017,731.185C36.268,687.194-2.448,575.044.546,402.782,4.106,197.973-20.06,138.523,67.017,42.622Z" transform="translate(770.513 0) rotate(90)" fill="#DDDEE0" />
-                    <path id="Tracé_9" data-name="Tracé 9" d="M1002.585,5430.063v185.7" transform="translate(-606.825 -5070.891)" fill="none" stroke="#DDDEE0" stroke-width="20" />
-                </svg> -->
-        </div>
-        <!-- <div > -->
-        <div class="map column is-2-fullhd" id="map">
-            <!-- Ici s'affichera la carte -->
-        </div>
-        <!-- </div> -->
-    </div>
-
-</section>
-
-
-<?php global $wpdb;
-// Interrogation de la base de données
-$resultats = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}options WHERE option_name = 'adress_client'");
-// Parcours des resultats obtenus
-foreach ($resultats as $post) {
-}
-?>
-<script>
-    var adress_client = '<?PHP echo $post->option_value; ?>';
-</script>
+    <?php global $wpdb;
+    // Interrogation de la base de données
+    $resultats = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}options WHERE option_name = 'adress_client'");
+    // Parcours des resultats obtenus
+    foreach ($resultats as $post) {
+    }
+    ?>
+    <script>
+        var adress_client = "<?PHP echo $post->option_value; ?>";
+    </script>
 </main>
 
 
