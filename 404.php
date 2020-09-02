881c69c4e3797c21b6f8022061ae3f854a685de9
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-09-02 19:46:01 +0200

    Ajout page 404

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php do_action('display_header', 'package_header'); ?>
+
+<main class="container">
+    <section id="page-error" class="my-5">
+
+        <h1 class="has-text-centered is-size-3 mb-6">Erreur 404 : page non trouvée !</h1>
+
+        <p class="has-text-centered is-size-5 mb-4">Référez-vous au menu ou cliquer sur le logo pour retourner sur la page d'accueil.</p>
+
+        <div class="has-text-centered my-6">
+            <img src="wp-content/themes/from_scratch/assets/img/404_page_not_found.svg" class="img-error" alt="Image représentant la page 404">
+        </div>
+
+    </section>
+</main>
+
+<?php do_action('display_footer', 'package_footer'); ?>
